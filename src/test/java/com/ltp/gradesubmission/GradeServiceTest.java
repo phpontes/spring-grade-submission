4e55fc7d5ee6cb11521af6010498035fd9554710
phpontes <ppontes95@hotmail.com>
2025-01-30 17:46:01 -0300

    Setting up Testing Class

---
+++ src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -0,0 +1,19 @@
+package com.ltp.gradesubmission;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.service.GradeService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GradeServiceTest {
+    
+    @Mock
+    private GradeRepository gradeRepository;
+
+    @InjectMocks
+    private GradeService gradeService;
+}
